0cd8a9221771ed790576e94a6f1e0d4c8260ae9c
tr202 <tr202@ya.ru>
2023-04-10 14:17:09 +0300

    Update models

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+from users.models import YamdbUser
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
 
     def __str__(self):
-        return self.category
+        return self.name
 
 
 class Genre(models.Model):
@@ -14,33 +16,49 @@ class Genre(models.Model):
     slug = models.SlugField(unique=True)
 
     def __str__(self):
-        return self.genre
+        return self.name
 
-'''
+
 class Title(models.Model):
-    name = models.CharField(max_length=200)
-    year = models.DateField
-    description = models.CharField(max_length=200)
-    description = models.TextField()
+    name = models.CharField(max_length=256)
+    year = models.DateField()
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, related_name='category')
+    description = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.title
+        return self.name
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL, related_name='genres')
+    title = models.ForeignKey(
+        Title, on_delete=models.SET_NULL, related_name='titles')
 
 
 class Review(models.Model):
-    review = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='title',)
+    text = models.TextField()
+    author = models.ForeignKey(
+        YamdbUser, on_delete=models.CASCADE, related_name='author',)
+    score = models.IntegerField(blank=True, null=True, related_name='рейтинг',)
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True, blank=True, null=True,)
 
     def __str__(self):
         return self.title
 
-   
+
 class Comment(models.Model):
-    review = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    author = models.ForeignKey(
+        YamdbUser, on_delete=models.CASCADE, related_name='author',)
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True, blank=True, null=True,)
 
     def __str__(self):
-        return self.title
-'''
+        return self.text[:100]
