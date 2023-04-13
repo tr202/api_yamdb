4d2337bd268a6b4aca570cd52e34cd8b75f97835
tr202 <tr202@ya.ru>
2023-04-13 10:50:54 +0300

    Sort imports

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,9 +1,6 @@
 from django.core.exceptions import ObjectDoesNotExist
 
-from rest_framework import filters
-from rest_framework import permissions
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.pagination import PageNumberPagination
