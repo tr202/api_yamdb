10c67e738b0677674a1a945ef2b278e3bf2620f7
tr202 <tr202@ya.ru>
2023-04-14 22:14:06 +0300

    update logic

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,19 +1,21 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+SAFE_METHODS_SET = set(SAFE_METHODS)
+
 
 class IsAdminModeratorOwnerOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
+        if request.method in SAFE_METHODS_SET:
             return True
         if bool(request.user and request.user.is_authenticated):
-            if request.method in ['POST', 'PATCH', 'DELETE']:
+            if request.method in {'POST', 'PATCH', 'DELETE'}:
                 return True
             return bool(request.user.role == 'admin'
                         or request.user.role == 'moderator')
         return False
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
+        if request.method in SAFE_METHODS_SET:
             return True
         if bool(request.user and request.user.is_authenticated):
             return bool(obj.author == request.user
@@ -24,7 +26,7 @@ class IsAdminModeratorOwnerOrReadOnly(BasePermission):
 
 class IsAdminRole(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
+        if request.method in SAFE_METHODS_SET:
             return True
         is_user = bool(request.user and request.user.is_authenticated)
         if is_user:
@@ -34,7 +36,7 @@ class IsAdminRole(BasePermission):
         return False
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
+        if request.method in SAFE_METHODS_SET:
             return True
         if bool(request.user and request.user.is_authenticated):
             return bool(request.user.role == 'admin'
@@ -44,7 +46,7 @@ class IsAdminRole(BasePermission):
 
 class IsAdminRoleOrStaff(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
+        if request.method in SAFE_METHODS_SET:
             return True
         is_user = bool(request.user and request.user.is_authenticated)
         if is_user:
@@ -54,7 +56,7 @@ class IsAdminRoleOrStaff(BasePermission):
         return False
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
+        if request.method in SAFE_METHODS_SET:
             return True
         is_user = bool(request.user and request.user.is_authenticated)
         if is_user:
